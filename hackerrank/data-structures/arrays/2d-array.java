e85e810c0cd749cb4787eba06efc75a935e11998
Pasang Sherpa <pasang_sh@hotmail.com>
2016-05-22 12:26:41 -0700

    solution for 2d-array

---
+++ hackerrank/data-structures/arrays/2d-array.java
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/2d-array
+
+public class Solution {
+  private static int largestHourGlassSum (int[][] arr) {
+    int largest, sum;
+    largest = sum = Integer.MIN_VALUE;
+    
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+        
+        if (largest < sum) largest = sum;
+      }
+    }
+    
+    return largest;
+  }
+  
+  public static void main (String[] args) {
+      Scanner in = new Scanner(System.in);
+      int arr[][] = new int[6][6];
+      for (int arr_i = 0; arr_i < 6; arr_i++){
+          for (int arr_j = 0; arr_j < 6; arr_j++){
+              arr[arr_i][arr_j] = in.nextInt();
+          }
+      }
+
+      System.out.println(largestHourGlassSum(arr));
+  }
+}
--- hackerrank/data-structures/arrays/arrays-ds.java
+++ hackerrank/data-structures/arrays/arrays-ds.java
@@ -3,13 +3,13 @@
 import java.util.*;
 
 public class Solution {
-    private static void printReverse(int[] arr) {
+    private static void printReverse (int[] arr) {
       for (int i = arr.length - 1; i >= 0; i--) {
         System.out.print(arr[i] + " ");
       }
     }
   
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int arr[] = new int[n];
