2ab5ff8d796df1adefa00a70a39e317462c54394
Pasang Sherpa <pasang_sh@hotmail.com>
2016-06-14 19:39:13 -0700

    kth smallest solution

---
+++ others/data-structures/maps/kth-smallest.java
@@ -0,0 +1,37 @@
+/**
+ * Given an array of inte­gers, find the Kth smallest/largest ele­ment
+ * in the array.
+ *
+ * Example:
+ * int[] A = { 1, 2, 10, 20, 40, 32, 44, 51, 6 };
+ * K = 4;
+ *
+ * 4th smallest element in given array: 10
+ */
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KthSmallest {
+  public static int kthSmallest(int[] nums, int k) {
+    Queue<Integer> q = new PriorityQueue<Integer>();
+
+    for (int num: nums) {
+      q.offer(num);
+    }
+
+    int kthSmallest = -1;
+    while (k-- > 0) {
+      kthSmallest = q.remove();
+    }
+
+    return kthSmallest;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = { 1, 2, 10, 20, 40, 32, 44, 51, 6 };
+    int k = 4;
+
+    System.out.println(kthSmallest(nums, k));
+  }
+}
