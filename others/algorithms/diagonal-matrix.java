5ddc3a68aea3dd8dc7abdbd7df43c07ce8b8bc51
Pasang Sherpa <pasang_sh@hotmail.com>
2016-06-18 14:38:25 -0700

    remove unnecessary comment

--- others/algorithms/diagonal-matrix.java
+++ others/algorithms/diagonal-matrix.java
@@ -26,13 +26,6 @@ public class Matrix {
   }
 
   public void printAllDiagonals() {
-    /**
-     * 00, 01, 02, 03
-     * 10, 11, 12, 13
-     * 20, 21, 22, 23
-     * 30, 31, 32, 33
-     */
-
     int tmp, row, col;
     for (row = 0; row < elements.length; row++) {
       tmp = row;
